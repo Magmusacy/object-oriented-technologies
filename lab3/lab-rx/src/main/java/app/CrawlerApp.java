3afcf4f0b70a03f7df4318320295f335af0c634b
Magmusacy <xpfk.098@gmail.com>
2025-10-20 18:14:11 +0200

    Finish work during labs on lab3

---
+++ lab3/lab-rx/src/main/java/app/CrawlerApp.java
@@ -0,0 +1,20 @@
+package app;
+
+import java.io.IOException;
+import java.util.List;
+
+public class CrawlerApp {
+
+    public static final String GOOGLE_CUSTOM_SEARCH_API_KEY = "AIzaSyABcALkIU-W7inP3DAzabFJgEfJAu-2gAI";
+
+    private static final List<String> TOPICS = List.of("Agent Cooper", "Sherlock", "Poirot", "Detective Monk", "Scooby Doo", "Herobrine minecraft");
+
+
+    public static void main(String[] args) throws IOException {
+        PhotoCrawler photoCrawler = new PhotoCrawler();
+        photoCrawler.resetLibrary();
+//        photoCrawler.downloadPhotoExamples();
+        photoCrawler.downloadPhotosForQuery(TOPICS.get(0));
+//        photoCrawler.downloadPhotosForMultipleQueries(TOPICS);
+    }
+}
